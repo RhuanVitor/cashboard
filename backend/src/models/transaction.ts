c1b82fbefdb55d95cb341d5624851cb3420fc4c2
RhuanVitor <rhuanvs72840@gmail.com>
2025-07-18 13:03:21 -0300

    Name added in transaction model

--- backend/src/models/transaction.ts
+++ backend/src/models/transaction.ts
@@ -2,15 +2,20 @@ import mongoose, { Schema, Document } from 'mongoose'
 
 export interface ITransaction extends Document {
   userId: string
+  name: string
   value: number
   date: Date
   category: string
 }
 
-const transactionSchema = new Schema<ITransaction>({
+const transactionSchema = new Schema<ITransaction>({ 
   userId: {
     type: String,
     required: true
+  },
+  name: {
+    type: String,
+    required: false
   },
   value: {
     type: Number,
