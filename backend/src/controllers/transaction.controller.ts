c76a691ee398af3163b5e7c9fbb6b62414883d1f
RhuanVitor <rhuanvs72840@gmail.com>
2025-07-21 12:30:24 -0300

    Functions created in transaction controller: createTransaction, updateTransaction, deleteTransaction

--- backend/src/controllers/transaction.controller.ts
+++ backend/src/controllers/transaction.controller.ts
@@ -1,17 +1,114 @@
 import { Request, Response } from "express";
 
-export async function getTransactions(req: Request, res: Response){
+import { ITransaction, Transaction } from "../models/transaction";
 
+export async function getTransactions(req: Request, res: Response){
+    
 }
 
 export async function createTransaction(req: Request, res: Response){
+    const { name, value, categoryId, date } = req.body;
+    const userId = (req as any).userId; 
 
+    if(!name){
+        return res.status(422).json({'msg': 'Name is required!'})
+    }
+
+    if(!value){
+        return res.status(422).json({'msg': 'Value is required!'})
+    }
+
+    if(!categoryId){
+        return res.status(422).json({'msg': 'Category ID is required!'})
+    }
+
+    if(!date){
+        return res.status(422).json({'msg': 'Date is required!'})
+    }
+
+    try{
+        const transaction = new Transaction({name, value, categoryId, userId, date});
+
+        await transaction.save();
+
+        res.status(201).json({'msg': 'Transaction added successfully!', transaction});
+    } catch (error){
+        res.status(500).json({
+            'msg': 'A server error occurred, please try again later.'
+        })
+
+        console.log(`A server error occurred: ${error}`);
+    }
 }
 
 export async function updateTransaction(req: Request, res: Response){
+    const transactionId = req.params.id;
+    const userId = (req as any).userId;
 
+    const { name, value, categoryId, date } = req.body
+
+    if(!name){
+        return res.status(422).json({'msg': 'Name is required!'})
+    }
+
+    if(!value){
+        return res.status(422).json({'msg': 'Value is required!'})
+    }
+
+    if(!categoryId){
+        return res.status(422).json({'msg': 'Category ID is required!'})
+    }
+
+    if(!date){
+        return res.status(422).json({'msg': 'Date is required!'})
+    }
+
+    if(!transactionId){
+        return res.status(422).json({'msg': 'The transaction ID is required as a URL parameter!'});
+    }
+
+    try{
+        const transaction = await Transaction.findOneAndUpdate(
+            { _id: transactionId, userId},
+            { name, value, categoryId, date },
+            { new: true }
+        );
+
+        if (!transaction) return res.status(404).json({ msg: 'Transaction not found or unauthorized' });
+
+        res.status(200).json({ msg: 'Updated!', transaction });
+
+    } catch (error){
+        res.status(500).json({
+            'msg': 'A server error occurred, please try again later.'
+        })
+
+        console.log(`A server error occurred: ${error}`);
+    }
 }
 
 export async function deleteTransaction(req: Request, res: Response){
+    const transactionId = req.params.id;
+    const userId = (req as any).userId;
 
+    if(!transactionId){
+        return res.status(422).json({'msg': 'The transaction ID is required as a URL parameter!'});
+    }
+
+    try{
+        const transaction = await Transaction.findOneAndDelete(
+            { _id: transactionId, userId},
+        );
+
+        if (!transaction) return res.status(404).json({ msg: 'Transaction not found or unauthorized' });
+
+        res.status(200).json({ msg: 'Transaction deleted!', transaction });
+
+    } catch (error){
+        res.status(500).json({
+            'msg': 'A server error occurred, please try again later.'
+        })
+
+        console.log(`A server error occurred: ${error}`);
+    }    
 }
