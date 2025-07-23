0f36f3ce5f8fd03920cf31536c57ad3919a9adab
RhuanVitor <rhuanvs72840@gmail.com>
2025-07-23 13:18:03 -0300

    Controller for categories finished

--- backend/src/controllers/category.controller.ts
+++ backend/src/controllers/category.controller.ts
@@ -34,7 +34,7 @@ export async function createCategory(req: Request, res: Response){
         const category = new Category({ userId, name, color});
         await category.save();
 
-        return res.status(201).json({ category });
+        return res.status(201).json({ 'msg': 'Category added successfully', category });
     } catch(error){
         res.status(500).json({
             'msg': 'A server error occurred, please try again later.'
@@ -48,9 +48,47 @@ export async function updateCategory(req: Request, res: Response){
     const { name, color } = req.body;
     const categoryId = req.params.id;
     const userId = (req as any).userId;
+
+    if(!name){
+        return res.status(422).json({'msg': 'Name is required!'});
+    }
+    
+    if(!color){
+        return res.status(422).json({'msg': 'Color is required!'})
+    }
+
+    try{
+        const category = await Category.findOneAndUpdate(
+            {_id: categoryId, userId }, 
+            { name, color}, 
+            { new: true }
+        );
+
+        if (!category) return res.status(404).json({ msg: 'Category not found or unauthorized' });
+
+        return res.status(200).json({ msg: 'Category updated!', category });
+    } catch(error){
+        res.status(500).json({
+            'msg': 'A server error occurred, please try again later.'
+        })
+
+        console.log(`A server error occurred: ${error}`);
+    }
 }
 
 export async function deleteCategory(req: Request, res: Response){
     const categoryId = req.params.id;
     const userId = (req as any).userId;
+
+    try{
+        const category = await Category.findOneAndDelete({ _id: categoryId, userId });
+
+        res.status(200).json({ msg: 'Category deleted!', category });
+    }catch(error){
+        res.status(500).json({
+            'msg': 'A server error occurred, please try again later.'
+        })
+
+        console.log(`A server error occurred: ${error}`);
+    }
 }
